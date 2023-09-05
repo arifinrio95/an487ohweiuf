e59d59bb76cdea0fc4cf8a5f46f85d04b66febab
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-09-05 16:01:13 +0700

    Update script.py

--- script.py
+++ script.py
@@ -49,7 +49,7 @@ def request_content(prompt):
         model="gpt-4",
         # model="gpt-3.5-turbo",
         messages=messages,
-        max_tokens=10000,
+        max_tokens=3000,
         temperature=0)
     script = response.choices[0].message['content']
     return script
