c031779c5faf198edc375eacfae18e2a8bb9f9e2
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-09-05 22:04:51 +0700

    Update script.py

--- script.py
+++ script.py
@@ -165,8 +165,8 @@ def main():
 
                         url = st.text_input("Masukkan link bukti sawer untuk melanjutkan. Masukkan link lengkap mulai dari 'https://'", key=f'btn_textinput1')
                         st.session_state.url = url
-                        if st.button("Submit", key = "button_url"):
-                            st.experimental_rerun()
+                        # if st.button("Submit", key = "button_url"):
+                        #     st.experimental_rerun()
 
         if 'url' in st.session_state:
             if check_word_in_url(st.session_state.url) == True:
