278a9f9916f44a5309c06b1f5594aaabc3bc110d
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-09-05 21:46:44 +0700

    Update script.py

--- script.py
+++ script.py
@@ -202,7 +202,7 @@ def main():
             if 'url' not in st.session_state:
                 url = st.text_input("Masukkan link bukti sawer untuk melanjutkan. Masukkan link lengkap mulai dari 'https://'", key=f'btn_textinput1')
                 st.session_state.url = url
-                if st.button_url:
+                if st.button("Submit"):
                     st.experimental_rerun()
                 
             # st.session_state.title = title
