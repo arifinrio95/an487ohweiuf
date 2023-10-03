c5c3c9df69661d6d2ff64f7ddab3c9819b1499d5
Rio Nur Arifin <59592379+arifinrio95@users.noreply.github.com>
2023-10-03 19:33:39 +0700

    Update script.py

--- script.py
+++ script.py
@@ -124,33 +124,33 @@ def main():
         if button:
             st.session_state.button_clicked = True
         
-    if 'button_clicked' in st.session_state and ml_model and special_topic:
-    # if button and ml_model and special_topic:
+    # if 'button_clicked' in st.session_state and ml_model and special_topic:
+    if button and ml_model and special_topic:
         prompt_1 = f"Berikan 10 ide judul skripsi tentang {ml_model}, {special_topic}. Beri nomor 1 - 10 pada setiap judul. Jangan berikan kalimat pengantar atau apapun kecuali judul. Langsung mulai dengan '1. (judul no 1)"
 
-        # if 'titles' in st.session_state:
-        #     # Menampilkan setiap judul sebagai text yang bisa diklik   
-        #     st.write("Klik pada judul untuk men-generate skripsi sederhana dari Bab 1-4.")
-        #     if ('special_topic' in st.session_state) and ('ml_model' in st.session_state): 
-        #         if (st.session_state.special_topic != special_topic) or (st.session_state.ml_model != ml_model):
-        #             st.session_state.button_submit2 = True
-        #             if st.button("Submit", key = 'btn_resubmit'):
-        #                 with st.spinner('Generating title ideas...'):
-        #                     titles = extract_titles(request_title(prompt_1, ml_model, special_topic))
-        #                     st.session_state.titles = titles
-        #                     for title in st.session_state.titles:
-        #                         button2 = st.button(title, key=f'btn_submit_{title}')
-        #                         if button2:
-        #                             st.button(title, key=f'btn_submit_{title}')
-        #                             st.session_state.button2_clicked = True
-        #                             st.session_state.title = title
-        #         else:
-        #             for title in st.session_state.titles:
-        #                 button2 = st.button(title, key=f'btn_submit_{title}')
-        #                 if button2:
-        #                     # st.button(title, key=f'btn_submit_{title}')
-        #                     st.session_state.button2_clicked = True
-        #                     st.session_state.title = title
+        if 'titles' in st.session_state:
+            # Menampilkan setiap judul sebagai text yang bisa diklik   
+            st.write("Klik pada judul untuk men-generate skripsi sederhana dari Bab 1-4.")
+            if ('special_topic' in st.session_state) and ('ml_model' in st.session_state): 
+                if (st.session_state.special_topic != special_topic) or (st.session_state.ml_model != ml_model):
+                    st.session_state.button_submit2 = True
+                    if st.button("Submit", key = 'btn_resubmit'):
+                        with st.spinner('Generating title ideas...'):
+                            titles = extract_titles(request_title(prompt_1, ml_model, special_topic))
+                            st.session_state.titles = titles
+                            for title in st.session_state.titles:
+                                button2 = st.button(title, key=f'btn_submit_{title}')
+                                if button2:
+                                    st.button(title, key=f'btn_submit_{title}')
+                                    st.session_state.button2_clicked = True
+                                    st.session_state.title = title
+                else:
+                    for title in st.session_state.titles:
+                        button2 = st.button(title, key=f'btn_submit_{title}')
+                        if button2:
+                            # st.button(title, key=f'btn_submit_{title}')
+                            st.session_state.button2_clicked = True
+                            st.session_state.title = title
                     
         if 'titles' not in st.session_state:
             with st.spinner('Generating title ideas...'):
